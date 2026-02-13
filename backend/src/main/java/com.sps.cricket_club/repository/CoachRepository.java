ce8c8a2e540c167c55c5c654b786e34f19c57cc7
Imashi-c <imashigunarathna25@gmail.com>
2026-02-13 13:40:55 +0530

    Update CoachRepository.java

--- backend/src/main/java/com.sps.cricket_club/repository/CoachRepository.java
+++ backend/src/main/java/com.sps.cricket_club/repository/CoachRepository.java
@@ -1,1 +1,12 @@
+package com.sps.cricket_club.repository;
 
+
+import com.sps.cricket_club.entity.Coach;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CoachRepository extends JpaRepository<Coach, Long> {
+    Optional<Coach> findByEmail(String email);
+}
